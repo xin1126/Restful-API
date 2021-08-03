2716ad3f8b3474cb6f81f26532f0c89f29381dae
XinUndfined <z26319491z@gmail.com>
2021-08-03 15:14:54 +0800

    crud

--- app.js
+++ app.js
@@ -13,9 +13,6 @@ admin.initializeApp({
 
 const fireData = admin.database();
 
-// app.set('views','./views');
-// app.set('view engine','ejs');
-// app.use(express.static('public'))
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
